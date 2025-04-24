cbb927cba18bdd45d05b6ad7efa6fdd283b3de80
AkhatorEnosa <aosakhogba@gmail.com>
2025-04-24 03:44:07 +0100

    card animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,76 @@
-import { IoMdEye, IoMdEyeOff } from "react-icons/io"
 import Card from "./components/Card"
 import { useContext } from "react";
 import { AppContext } from "./context/AppContext";
+import { motion } from "framer-motion";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   const { showAll, setShowAll } = useContext(AppContext);
 
+  const animationVariants = {
+    inactive: { 
+      transition: { 
+        duration: 1,
+        staggerChildren: 0.05,
+        bounce: 1,
+        ease: "easeOut",
+      } },
+    active: {
+      transition: { 
+        duration: 6,
+        staggerChildren: 0.05,
+        bounce: 1,
+        ease: "easeOut",
+      } },
+  }
+
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-green-100">
-      <div className="relative w-[40%] h-[70%] flex flex-col justify-center items-center gap-2 p-2 bg-white rounded-t-3xl rounded-b-[80px] shadow-2xl overflow-hidden">
-        <div className="bg-black/80 absolute w-[400px] h-[50px] bottom-20 blur-2xl z-0"></div>
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="relative w-[40%] h-[70%] flex flex-col justify-center items-center gap-2 bg-white rounded-t-3xl rounded-b-[80px] shadow-2xl">
+        <div className="absolute w-full h-full opacity-40">
+          <div className="absolute bg-amber-200 size-[60%] rotate-12 left-0 top-0 blur-3xl"></div>
+          <div className="absolute bg-green-300 size-[60%] rotate-12 right-0 bottom-0 blur-3xl"></div>
+        </div>
+        <div className={`bg-black/95 absolute w-[400px] h-[50px] bottom-20 blur-2xl ${showAll ? "opacity-80" : "opacity-100"} delay-300 transitiona-all duration-300 z-0`}></div>
         <div className="relative bg-[#293c1e] w-[65%] h-[65%] rounded-t-3xl rounded-b-[65px] flex justify-center items-center">
-          <div className="cards w-full h-full flex flex-col justify-center items-center gap-2">
+          <motion.div
+          variants={animationVariants}
+          animate={showAll ? "active" : "inactive"}
+          className="cards w-full h-full flex flex-col justify-center items-center gap-2">
             <Card 
-              imgSrc="stripe.png"
-              value={showAll ? "$32,495" : "******"}
-              variants="-top-7 bg-[#A5A7EE] text-white"
-              imgSize="w-[20%]"
+              imgSrc="paypal.png"
+              variants="bg-[#d6d6d4] text-[#797a77] z-30"
+              value={"$345,865"}
+              imgSize="w-[25%]"
             />
             <Card 
               imgSrc="wise.png"
-              value={showAll ? "$45,654" : "******"}
-              variants="top-3 bg-[#91D46C] text-[#797a77]"
+              value={"$45,654"}
+              variants={`${showAll ? "top-0" : "top-3"} bg-[#91D46C] text-[#797a77] z-20`}
               imgSize="w-[20%]"
             />
             <Card 
-              imgSrc="paypal.png"
-              variants="bg-[#d6d6d4] text-[#797a77]"
-              value={showAll ? "$345,865" : "******"}
-              imgSize="w-[25%]"
+              imgSrc="stripe.png"
+              value={"$32,495"}
+              variants={`${showAll ? "-top-15" : "-top-7"} bg-[#A5A7EE] text-white z-10`}
+              imgSize="w-[20%]"
             />
-          </div>
+          </motion.div>
 
-          <div className="pocket flex flex-col items-center justify-between absolute bg-[#1F2F16] w-[95%] h-[68%] bottom-2 left-[10] py-5 border-2 border-[#45583a] border-dashed rounded-b-[65px] text-white">
-            <div className="relative w-full h-fit flex flex-col justify-center items-center text-white/50">
-              <p className={`total flex justify-center items-center font-extralight text-3xl ${showAll ? "text-neutral-300 tracking-normal" : "text-white/50 tracking-widest"}`}>{showAll ? "424,014" : "******"}</p>
+          <div className="pocket flex flex-col items-center justify-between absolute bg-[#1F2F16] w-[95%] h-[68%] bottom-2 left-[10] py-5 border-2 border-[#45583a] border-dashed  inset-shadow-sm inset-shadow-white/50 rounded-t-[8px] rounded-b-[65px] text-white z-50 overflow-clip">
+            <div className="absolute pattern w-full h-full z-30"></div>
+            <div className="relative w-full h-fit flex flex-col justify-center items-center text-white/50 z-40">
+              <p className={`total relative w-full flex justify-center items-center font-extralight text-3xl ${showAll ? "text-neutral-300 tracking-normal" : "text-white/50 tracking-widest"} overflow-clip`}>
+                <span className={`${showAll ? "scale-0 opacity-0 -translate-y-10" : "scale-100 opacity-100 translate-y-0"} delay-300 transition-all duration-300`}>******</span>
+                <span className={`${showAll ? "scale-100 translate-y-0 opacity-100" : "scale-0 translate-y-20 opacity-0"} absolute delay-300 transition-all duration-300`}>$424,014</span>
+              </p>
               <p className="text-sm">Total Balance</p>
             </div>
-            <div className="text-4xl" onClick={() => setShowAll(!showAll)}>
-              {showAll ? <IoMdEye className="text-[#7aab62]"/> :
-              <IoMdEyeOff className="text-[#7aab62]/20"/>}
+            <div className="relative flex justify-center items-center text-4xl z-40" onClick={() => setShowAll(!showAll)}>
+
+                <i className={`bi bi-eye-slash-fill text-[#7aab62]/50 ${!showAll ? "scale-100" : "scale-0"} cursor-pointer delay-300 transition-all duration-400`}></i>
+                <i className={`absolute bi bi-eye-fill text-[#7aab62]/80 ${showAll ? "scale-100" : "scale-0"} cursor-pointer delay-300 transition-all duration-400`}></i>
+
             </div>
           </div>
         </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,7 @@
+import { motion } from 'framer-motion';
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
 interface CardProps {
     imgSrc: string;
     value: string;
@@ -6,13 +10,36 @@ interface CardProps {
 }
 
 const Card = ({imgSrc, value, variants, imgSize}: CardProps) => {
+    const {showAll} = useContext(AppContext);
+
+
+    const motionVariants = {
+        inactive: {
+        y: [0, -120, 50, 0],
+        scale: [1, 1, 0.9, 0.9, 1],
+        rotate: [0, 0, 0, 0, 5],
+        },
+        active: {
+            scale: [1, 0.9, 0.9, 1, 1],
+            y: [0, 40, -120, 0], // Keyframe animation
+            rotate: [5, 5, 5, 0, 0], // Keyframe animation
+        },
+    };
+
   return (
-    <div className={`card absolute w-[90%] h-[70%] ${variants} rounded-3xl flex justify-center inset-shadow-sm inset-shadow-white`}>
+    <motion.div
+    // initial={{ y: 0, scale: 1 }}
+    variants={motionVariants}
+    // animate={showAll ? "active" : "inactive"} 
+    className={`card absolute w-[90%] h-[70%] ${variants} rounded-3xl flex justify-center inset-shadow-sm inset-shadow-white`}>
       <div className="w-full h-fit flex justify-between items-center px-5 py-3">
         <img src={`./${imgSrc}`} alt="" className={imgSize}/>
-        <p className="">{value}</p>
+        <p className="relative w-fit flex justify-center items-center">
+            <span className={`${showAll ? "scale-0 opacity-0 -translate-y-10" : "scale-100 opacity-100 translate-y-0"} delay-300 transition-all duration-300`}>******</span>
+            <span className={`${showAll ? "scale-100 translate-y-0 opacity-100" : "scale-0 translate-y-20 opacity-0"} absolute delay-300 transition-all duration-300`}>{value}</span>
+        </p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
